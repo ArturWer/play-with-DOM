641403df79f98157a7a390c6f77435db829e4db1
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-01-05 10:01:18 +0200

    Added js code with addEventListener

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
 "use strict"
+let ul = document.querySelector("ul");
+let h1 = document.querySelector("h1");
+ul.addEventListener("click", (e)=>{
+	let text = e.target.textContent;
+	h1.textContent = text;
+	console.log(text);
+});
