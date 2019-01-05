31095f848d07585dee129e3ea992284a4852dae8
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-01-05 16:28:52 +0200

    Added className Active for clicked list Item

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,13 @@ let btn = document.querySelector("button");
 let i = 1;
 ul.addEventListener("click", (e)=>{
 	let text = e.target.textContent;
+	let listItems = document.querySelectorAll("ul>li");
 	h1.textContent = text;
+	ul.className = "";
+	listItems.forEach((elem)=>{
+		elem.className = "";
+	});
+	e.target.className = "active";
 	console.log(text);
 });
 
