ea52e7d7b7a606dab8afc63ecd78ca57b82aa996
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-01-05 10:28:26 +0200

    Added adding New list items

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,17 @@
 "use strict"
 let ul = document.querySelector("ul");
 let h1 = document.querySelector("h1");
+let btn = document.querySelector("button");
+let i = 1;
 ul.addEventListener("click", (e)=>{
 	let text = e.target.textContent;
 	h1.textContent = text;
 	console.log(text);
+});
+
+btn.addEventListener("click", (e)=>{
+	e.preventDefault();
+	let li = document.createElement("li");
+	li.textContent = `New item ${i++}`;
+	ul.appendChild(li);
 });
